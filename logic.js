9e42c982de3b3881fe58d76ba286114214bb39cc
DanielZatorski <zatorskidaniel@gmail.com>
2025-06-19 19:58:11 +0200

    game iteration

--- logic.js
+++ logic.js
@@ -7,7 +7,7 @@ function getComputerChoice(){
 
 
 
- console.log(rand)
+ console.log(`Computer: ${rand}`)
  return rand
 
 }
@@ -26,7 +26,7 @@ function getHumanChoice(){
 
     let input_ = input.toLowerCase()
 
-    console.log(input_)
+    console.log(`Human: ${input_}`)
     return input_
 
 }
@@ -36,51 +36,82 @@ function getHumanChoice(){
 
 
 
-function playRound(){
+function playRound(humanSelection, computerSelection){
+        
+    if (humanSelection === "paper"&& computerSelection === "rock") {
+        console.log("Human Wins");
+        return "human";
+    } else if (humanSelection === "paper"&& computerSelection === "paper") {
+        console.log("Remis");
+        return "draw";
+    } else if (humanSelection === "paper"&& computerSelection === "scissors") {
+        console.log("Computer Wins");
+        return "computer";
+    } else if (humanSelection === "scissors"&& computerSelection === "scissors") {
+        console.log("Remis");
+        return "draw";
+    } else if (humanSelection === "scissors"&& computerSelection === "rock") {
+        console.log("Computer Wins");
+        return "computer";
+    } else if (humanSelection === "scissors"&& computerSelection === "paper") {
+        console.log("Human Wins");
+        return "human";
+    } else if (humanSelection === "rock"&& computerSelection === "rock") {
+        console.log("Remis");
+        return "draw";
+    } else if (humanSelection === "rock"&& computerSelection === "paper") {
+        result = console.log("Computer Wins")
+        return "computer";
+    } else if (humanSelection === "rock"&& computerSelection === "scissors") {
+        console.log("Human Wins")
+        return "human";
+
+    } 
+
+}
+
+function game(){
+
 
 
     let humanScore = 0
 
     let computerScore = 0
 
+    //limit to 5 for i
+    for (let i=0; i<5; i++) {
+        console.log(`\nRound ${i + 1}`);
 
-        
-    if (humanSelection === "paper"&& computerSelection === "rock") {
-        console.log("Human Wins")
-        humanScore++;
-    } else if (humanSelection === "paper"&& computerSelection === "paper") {
-        console.log("Remis");
-    } else if (humanSelection === "paper"&& computerSelection === "scissor") {
-        console.log("Computer Wins")
-        computerScore++;
-
-     } else if (humanSelection === "scissor"&& computerSelection === "scissor") {
-        console.log("Remis");
-    } else if (humanSelection === "scissor"&& computerSelection === "rock") {
-        console.log("Computer Wins")
-        computerScore++;
-            } else if (humanSelection === "scissor"&& computerSelection === "paper") {
-        console.log("Human Wins")
-         humanScore++;
-     } else if (humanSelection === "rock"&& computerSelection === "rock") {
-        console.log("Remis");
-    } else if (humanSelection === "rock"&& computerSelection === "paper") {
-        console.log("Computer Wins")
-        computerScore++;
-    } else if (humanSelection === "rock"&& computerSelection === "scissor") {
-        console.log("Human Wins")
-        humanScore++;
-
-
-
-
-  
-    }
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
     
+        const result = playRound(humanSelection,computerSelection)
+
+        if (result === "human"){
+
+           humanScore++; 
+        }
+        
+        else if 
+
+            (result === "computer"){
+
+                computerScore++
+            
+        }
+        
+    }
+
+    console.log(`\nFinal Score:\nHuman: ${humanScore}\nComputer: ${computerScore}`);
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+
+
+
+
+
+
+
+game();
